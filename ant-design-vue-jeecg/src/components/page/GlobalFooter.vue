ccd051ad405fd54b26a4fe9664981e42e6066f7d
goldencicada <875921355@qq.com>
2023-07-13 14:16:06 +0800

    修改备案号

--- ant-design-vue-jeecg/src/components/page/GlobalFooter.vue
+++ ant-design-vue-jeecg/src/components/page/GlobalFooter.vue
@@ -2,7 +2,7 @@
   <div class="footer">
     Copyright &copy; 2023 <a href="http://qsllwl.cn" target="_blank">新疆强盛冷链物流</a> 出品
     <br>
-    <a href="https://beian.miit.gov.cn/" target="_blank">新ICP备2022000657号</a>
+    <a href="https://beian.miit.gov.cn/" target="_blank">新ICP备2022000657号-2</a>
   </div>
 </template>
 
